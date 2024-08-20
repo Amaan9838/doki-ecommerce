8a59e1807c0cb1fd8908175ac936630cd44273bb
Amaan9838 <amaanmohd115@gmail.com>
2024-08-21 02:00:31 +0530

    update

--- src/app/_components/GridSection.js
+++ src/app/_components/GridSection.js
@@ -102,15 +102,20 @@
 'use client';
 import React from 'react';
 import { motion, useInView } from 'framer-motion';
+import { Lora } from "next/font/google";
+
+const ubuntu = Lora({ subsets: ["latin-ext"], weight: ["400",'500',"700"] ,
+  variable: '--font-ubuntu'
+});
 
 const GridSection = () => {
   return (
-    <section className="mx-auto p-4">
+    <section className={` ${ubuntu.variable} font-sans mx-auto p-4`}>
       <div className="flex md:flex-row flex-col gap-4">
         {/* Top Left */}
         <div className='relative flex flex-row md:flex-col gap-4'>
-          <AnimatedGridItem src="/tshirt.jpg" label="HALDI" />
-          <AnimatedGridItem src="/jacket.jpg" label="SANGEET" />
+          <AnimatedGridItem src="/tshirt.jpg" label="Elegance" />
+          <AnimatedGridItem src="/jacket.jpg" label="Be cool, be fashionable" />
         </div>
 
         {/* Top Middle (Autoplay Video) */}
@@ -129,7 +134,7 @@ const GridSection = () => {
             className="w-full h-full object-cover md:min-w-[250px]"
           ></video>
           <div className="absolute inset-0 flex items-center justify-center">
-            <span className="text-white text-3xl font-bold">WEDDING</span>
+            <span className={`${ubuntu.variable} font-sans text-white text-3xl font-bold`}>Find your signature look</span>
           </div>
         </motion.div>
 
@@ -141,11 +146,11 @@ const GridSection = () => {
         
           <AnimatedGridItem
             src="https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2F31%2F0d%2F310dbe8e29ea91d278a4ec73c137304e30bc8fb1.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BLOOKBOOK%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/main]"
-            label="MEHENDI"
+            label="Dress to Express"
           />
           <AnimatedGridItem
             src="https://img.freepik.com/premium-photo/young-man-red-coat-white-shirt-gray-beackground_73107-1699.jpg?w=360"
-            label="RECEPTION"
+            label="Fashion Frolic"
           />
         </div>
       </div>
@@ -172,7 +177,7 @@ const AnimatedGridItem = ({ src, label }) => {
         className="w-full h-full object-cover"
       />
       <div className="absolute inset-0 flex items-center justify-center">
-        <span className="text-white text-3xl font-bold">{label}</span>
+        <span className={` ${ubuntu.variable} font-sans text-white text-3xl font-semibold`}>{label}</span>
       </div>
     </motion.div>
   );
